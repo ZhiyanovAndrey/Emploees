1011e47b372708e067d12d92de7f43c972d821fc
Андрей Жиянов <aazhiyanov@gmail.com>
2024-02-16 13:10:41 +0300

    add class Context

---
+++ Models/Context.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace Emploees.Models
+{
+    public class Context:DbContext
+    {
+        public DbSet<Emploees> Emploees { get; set; } 
+
+        public Context()
+        {
+            //Database.EnsureDeleted(); // удаляет БД 
+            Database.EnsureCreated(); // создает БД
+            //Database.Migrate(); // Применение созданной миграции
+        }
+
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+
+        //    optionsBuilder.UseNpgsql("Host=localhost;Port=5433;Database=Manager;Username=postgres;Password=User1234")
+        //        //.LogTo(Console.WriteLine) // можно подсмотреть сформированный EF запрос
+        //        ;
+        //    /*Строка подключения содержит адрес сервера (параметр Host), порт (Port), 
+        //     * название базы данных на сервере (Database),
+        //     * имя пользователя в рамках сервера PostgreSQL (Username) и его пароль (Password).*/
+        //}
+    }
+}
---
+++ Models/Emploees.cs
@@ -0,0 +1,18 @@
+﻿namespace Emploees.Models
+{
+    public class Emploees
+    {
+        public int Id { get; set; }
+        public string SureName { get; set; }
+        public string Name { get; set; }
+        public string ThirdName { get; set; }
+        public DateTime BirthDay { get; set; }
+        public DateTime EmploymentDate{ get; set; }
+        public int Salary { get; set; }    
+
+            
+
+        //- “Создание/удаление/редактирование сотрудника”
+
+    }
+}
